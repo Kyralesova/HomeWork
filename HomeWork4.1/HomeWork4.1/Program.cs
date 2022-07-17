5b2e29b3e4e7ae9a248a5d3dd093c77b2facab99
Kyralesova <109028438+Kyralesova@users.noreply.github.com>
2022-07-17 16:06:49 +0300

    Add files via upload

---
+++ HomeWork4.1/HomeWork4.1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace HomeWork4._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество строк: ");
+            int line = int.Parse(Console.ReadLine());
+
+            Console.Write("Введите количество столбцов: ");
+            int column = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[line, column];
+
+            Random r = new Random();
+
+            int sum = 0;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = r.Next(10);
+                    sum += matrix[i, j];
+                    Console.Write($"{matrix[i, j],3} ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine($"Сумма всех чисел = {sum}");
+
+            Console.ReadKey();
+        }
+    }
+}
